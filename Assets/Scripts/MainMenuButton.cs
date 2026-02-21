f2f302a9ddcad018d043ffefa8a4745cbd7797f5
zak grivell <zak@grivell.uk>
2026-02-21 02:43:15 +0000

    my best attempt at animation (mildly cooked need help)

--- Assets/Scripts/MainMenuButton.cs
+++ Assets/Scripts/MainMenuButton.cs
@@ -1,10 +1,26 @@
+using System.Collections;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuButton: MonoBehaviour
 {
+    public Animator transition;
+
     public void startGame()
     {
-        SceneManager.LoadScene(0);
+        StartCoroutine(LoadNextLevel(1));
+    }
+
+    IEnumerator LoadNextLevel(int idx)
+    {
+        transition.SetTrigger("start");
+
+        transition.Update(0);
+
+        yield return new WaitForSeconds(1);
+
+
+        SceneManager.LoadScene(idx);
     }
 }
